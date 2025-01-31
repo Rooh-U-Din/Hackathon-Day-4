bfe35251783bcbbde86483509d26339ecf93ff01
Rooh-U-Din <fidajokhio2@gmail.com>
2025-01-31 09:01:31 -0800

    updated

--- src/app/component/footer-all.tsx
+++ src/app/component/footer-all.tsx
@@ -1,67 +1,125 @@
-import Image from "next/image"
-import Link from "next/link"
-
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer2 = () => {
-    return (
-        <div className=" hidden md:block md:w-[1170] lg:w-[1280] h-[380] max-w-[1440] xl:w-[1440]  bg-[rgb(42,37,75)]">
-            <div className="absolute flex gap-[100] mt-[58] ml-[82]">
+  return (
+    <div className="hidden md:block w-full bg-[rgb(42,37,75)] py-10">
+      <div className="container mx-auto flex flex-wrap justify-between px-6">
+        <div className="flex flex-col space-y-3 text-white">
+          <h3 className="text-base font-clash">Menu</h3>
+          <ul className="space-y-2">
+            <li className="text-sm font-satoshi">New arrivals</li>
+            <li className="text-sm font-satoshi">Best sellers</li>
+            <li className="text-sm font-satoshi">Recently viewed</li>
+            <li className="text-sm font-satoshi">Popular this week</li>
+            <li className="text-sm font-satoshi">All products</li>
+          </ul>
+        </div>
 
-                <ul className="text-white font-sans space-y-3">
-                    <li className="w-[84] h-[20] text-base font-clash">Manu</li>
-                    <li className="text-sm">New arrivals</li>
-                    <li className="text-sm font-satoshi">Best sellers</li>
-                    <li className="text-sm font-satoshi">Recently viewed</li>
-                    <li className="text-sm font-satoshi">Popular this week</li>
-                    <li className="text-sm font-satoshi">All products</li>
-                </ul>
+        <div className="flex flex-col space-y-3 text-white">
+          <h3 className="text-base font-clash">Categories</h3>
+          <ul className="space-y-2">
+            <li className="text-sm font-satoshi">Plant pots</li>
+            <li className="text-sm font-satoshi">Ceramics</li>
+            <li className="text-sm font-satoshi">Tables</li>
+            <li className="text-sm font-satoshi">Chairs</li>
+            <li className="text-sm font-satoshi">Crockery</li>
+            <li className="text-sm font-satoshi">Tableware</li>
+            <li className="text-sm font-satoshi">Cutlery</li>
+          </ul>
+        </div>
 
-                <ul className="text-white font-sans space-y-3">
-                    <li className="w-[84] h-[20] text-base font-clash">Categories</li>
-                    <li className="text-sm font-satoshi">Plant pots</li>
-                    <li className="text-sm font-satoshi">Ceramics</li>
-                    <li className="text-sm font-satoshi">Tables</li>
-                    <li className="text-sm font-satoshi">Chairs</li>
-                    <li className="text-sm font-satoshi">Crockery</li>
-                    <li className="text-sm font-satoshi">Tableware</li>
-                    <li className="text-sm font-satoshi">Cutlery</li>
-                </ul>
+        <div className="flex flex-col space-y-3 text-white">
+          <h3 className="text-base font-clash">Our company</h3>
+          <ul className="space-y-2">
+            <li className="text-sm font-satoshi">About us</li>
+            <li className="text-sm font-satoshi">Vacancies</li>
+            <li className="text-sm font-satoshi">Contact us</li>
+            <li className="text-sm font-satoshi">Privacy</li>
+            <li className="text-sm font-satoshi">Returns policy</li>
+          </ul>
+        </div>
 
-                <ul className="text-white font-sans space-y-3 w-[105] h-[175]">
-                    <li className="w-[105] h-[20] text-base font-clash">Our company</li>
-                    <li className="text-sm font-satoshi">About us</li>
-                    <li className="text-sm font-satoshi">Vacancies</li>
-                    <li className="text-sm font-satoshi">Contact us</li>
-                    <li className="text-sm font-satoshi">Privacy</li>
-                    <li className="text-sm font-satoshi">Returns policy</li>
-                </ul>
-                </div>
-<div>
-                <span className="ml-[730] mt-[58] absolute text-white font-clash">Join our mailing list</span>
-                <div className="absolute">
-                <input type="email" placeholder="your@email.com" className="font-satoshi bg-[rgba(78,77,147,1)] placeholder:pl-7 h-[56]  md:ml-[580] md:mt-[180] md:w-[127] mt-[94] lg:w-[307] lg:ml-[630] xl:w-[527] xl:ml-[730]" />
-                <button className=" font-satoshi lg:w-[118] lg:h-[56] md:w-[60] md:h-[56]  bg-white">Sign up</button>
-                </div>
-            </div>
+        <div className="flex flex-col space-y-3 text-white">
+          <h3 className="text-base font-clash">Join our mailing list</h3>
+          <div className="flex mt-4">
+            <input
+              type="email"
+              placeholder="your@email.com"
+              className="font-satoshi bg-[rgba(78,77,147,1)] placeholder:pl-7 h-[56px] w-full max-w-xs rounded-l-md"
+            />
+            <button className="font-satoshi bg-white text-black px-4 rounded-r-md">
+              Sign up
+            </button>
+          </div>
+        </div>
+      </div>
 
-            <div>
-                <Image src="/photos/footerline.png" alt="" height={200} width={200} className="absolute xl:w-[1277] md:w-[700] h-px mt-[350] ml-[80]"></Image>
-                <span className="font-satoshi text-white absolute mt-[355] ml-[80]">Copyright 2022 Avion LTD</span>
-            </div>
-            <div className="absolute flex gap-[24] mt-[355] xl:ml-[1093] md:ml-[500] ">
-            <Image src="/photos/linkedin.png" alt="" height={200} width={200} className="w-[24] h-[24] "></Image>
-            <Image src="/photos/facebook.png" alt="" height={200} width={200} className="w-[24] h-[24] "></Image>
-            <Image src="/photos/instagram.png" alt="" height={200} width={200} className="w-[24] h-[24] "></Image>
-            <Image src="/photos/skype.png" alt="" height={200} width={200} className="w-[24] h-[24] "></Image>
-            <Image src="/photos/twitter.png" alt="" height={200} width={200} className="w-[24] h-[24] "></Image>
-            <Image src="/photos/pinterest.png" alt="" height={200} width={200} className="w-[24] h-[24]"></Image>
-            </div>
-            <Link href="/FAQs" passHref className="absolute mt-[250]"><button className=" font-satoshi w-[342] h-[36] ml-[1210] text-black bg-gray-100 mt-[32] md:w-[170] hover:text-blue-700">FAQs</button></Link>
-            </div>
+      <div className="container mx-auto mt-10 px-6">
+        <Image
+          src="/photos/footerline.png"
+          alt="divider"
+          height={1}
+          width={1277}
+          className="w-full h-px"
+        />
+        <div className="flex justify-between items-center mt-4">
+          <span className="font-satoshi text-white">
+            Copyright 2022 Avion LTD
+          </span>
+          <div className="flex gap-4">
+            <Image
+              src="/photos/linkedin.png"
+              alt="LinkedIn"
+              height={24}
+              width={24}
+              className="w-6 h-6"
+            />
+            <Image
+              src="/photos/facebook.png"
+              alt="Facebook"
+              height={24}
+              width={24}
+              className="w-6 h-6"
+            />
+            <Image
+              src="/photos/instagram.png"
+              alt="Instagram"
+              height={24}
+              width={24}
+              className="w-6 h-6"
+            />
+            <Image
+              src="/photos/skype.png"
+              alt="Skype"
+              height={24}
+              width={24}
+              className="w-6 h-6"
+            />
+            <Image
+              src="/photos/twitter.png"
+              alt="Twitter"
+              height={24}
+              width={24}
+              className="w-6 h-6"
+            />
+            <Image
+              src="/photos/pinterest.png"
+              alt="Pinterest"
+              height={24}
+              width={24}
+              className="w-6 h-6"
+            />
+          </div>
+        </div>
+        <Link href="/FAQs" passHref>
+          <button className="font-satoshi w-[170px] h-[36px] mt-4 bg-gray-100 text-black hover:text-blue-700">
+            FAQs
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-
-        
-
-    )
-}
-export default Footer2
+export default Footer2;
