7043152f9f6f634692e431acd1fdb35d7ff8d463
Rooh-U-Din <fidajokhio2@gmail.com>
2025-01-31 10:53:47 -0800

    updated

--- src/app/component/ProductActions.tsx
+++ src/app/component/ProductActions.tsx
@@ -22,21 +22,23 @@ const ProductActions: React.FC<{ product: Product }> = ({ product }) => {
   };
 
   return (
-    <div className=" flex md:block justify-between items-center mt-6 ">
-        
-      <button
-        className="w-[150] md:w-[300] bg-blue-700 h-8 rounded text-white hover:bg-blue-800 flex items-center justify-center hover:scale-110 transition-transform duration-300 ease-in-out font-satoshi"
-        onClick={handleAddToCart}
-      >
-        Add to cart
-      </button>
-      <br />
-      <Link href="/shopping" passHref>
-        <button className="w-[150] md:w-[300] bg-blue-700 h-8 rounded text-white hover:bg-blue-800 flex items-center justify-center hover:scale-110 transition-transform duration-300 ease-in-out font-satoshi">
-          Go to cart
-        </button>
-      </Link>
-    </div>
+    <div className="flex md:block justify-between items-center mt-6">
+  {/* Add to Cart Button */}
+  <button
+    className="w-[150px] md:w-[300px] bg-blue-700 h-8 rounded text-white hover:bg-blue-800 flex items-center justify-center hover:scale-110 transition-transform duration-300 ease-in-out font-satoshi"
+    onClick={handleAddToCart}
+  >
+    Add to cart
+  </button>
+  <br />
+
+  {/* Go to Cart Button */}
+  <Link href="/shopping" passHref>
+    <button className="w-[150px] md:w-[300px] bg-blue-700 h-8 rounded text-white hover:bg-blue-800 flex items-center justify-center hover:scale-110 transition-transform duration-300 ease-in-out font-satoshi">
+      Go to cart
+    </button>
+  </Link>
+</div>
   );
 };
 
